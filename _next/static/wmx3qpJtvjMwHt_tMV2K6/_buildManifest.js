345e191c0fc586fe02abdd95971baef3f703e2e1
alparslan166 <alp84202@gmail.com>
2026-02-21 22:37:18 +0300

    Updates

--- _next/static/wmx3qpJtvjMwHt_tMV2K6/_buildManifest.js
+++ _next/static/wmx3qpJtvjMwHt_tMV2K6/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/mektup//_next/:path+",
-        "destination": "/mektup/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
