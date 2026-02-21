1bf1a5d050b4746a9d716eed6b58bb6fb6393706
alparslan166 <alp84202@gmail.com>
2026-02-21 22:37:45 +0300

    l

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   basePath: "/mektup",
-  assetPrefix: "/mektup/",
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
